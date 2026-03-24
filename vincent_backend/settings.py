5cbc1ffac010ea1a90b2387642d664ac376c3b82
Kaikai Vincent <kaikaivincent24@gmail.com>
2026-03-24 22:19:15 +0300

    Adding production  origin

--- vincent_backend/settings.py
+++ vincent_backend/settings.py
@@ -124,11 +124,13 @@ MEDIA_URL  = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # ── CORS ───────────────────────────────────────────────────────────────────────
+
 CORS_ALLOWED_ORIGINS = config(
     'CORS_ALLOWED_ORIGINS',
-    default='http://localhost:5173,http://localhost:3000',
+    default='http://localhost:5173,http://localhost:3000,https://kaikaivincent.vercel.app',
     cast=lambda v: [h.strip() for h in v.split(',')]
 )
+
 CORS_ALLOW_CREDENTIALS = True
 
 # ── Django REST Framework ──────────────────────────────────────────────────────
